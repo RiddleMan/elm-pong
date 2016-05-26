c4d43fb585f504c461a54f1417d720e0b904ddb9
Artur Ptaszek <artur.ptaszek@gmail.com>
2016-05-26 15:52:03 +0200

    Init

---
+++ Ball.elm
@@ -0,0 +1,12 @@
+module Ball exposing (view, update, Model, init, subscriptions)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (on)
+
+view = div [] []
+
+type alias Model =
+  
+
+update :
