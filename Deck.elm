9d34f737371cb868e41be49e6017b71291b09543
Artur Ptaszek <artur.ptaszek@gmail.com>
2016-05-27 12:35:06 +0200

    Renames decker to deck

--- Deck.elm
+++ Deck.elm
@@ -1,28 +1,139 @@
-module Deck exposing (view, update, Model, init, subscriptions)
+module Deck exposing (view, update, Model, init, subscriptions, Msg)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Time exposing (..)
 import Window
 import Task
 import Util exposing (..)
+import Keyboard exposing (..)
+import Time exposing (Time)
 
-type Direction = Up
-	| Down
+type Direction =
+    Up
+    | Down
 
-type Msg = Run
-	| Stop
-	| Move Direction
-	| Screen Window.Size
+type Msg =
+    Run
+    | Stop
+    | MoveStart KeyCode
+    | MoveEnd KeyCode
+    | Tick Time
+    | Screen Int
+
+type alias Move =
+    Maybe Direction
+
+type alias Model =
+    { position: Float
+    , initialized: Bool
+    , height: Int
+    , screen: Int
+    , velocity: Float
+    , upKey: KeyCode
+    , downKey: KeyCode
+    , moveable: Bool
+    , moving: Move
+    , direction: Bool
+    }
+
+type alias Controls =
+    { up: KeyCode
+    , down: KeyCode
+    }
+
+getScreenDim : Cmd Msg
+getScreenDim =
+    Task.perform Screen Screen Window.height
+
+init : Controls -> Bool -> (Model, Cmd Msg)
+init { up, down } d =
+    ({ position = 0
+    , height = 70
+    , screen = 0
+    , velocity = 5
+    , initialized = False
+    , moveable = True
+    , upKey = up
+    , downKey = down
+    , moving = Nothing
+    , direction = d
+    }, getScreenDim)
+
+moveDeck : Float -> Direction -> Int -> Float -> Float
+moveDeck position direction screen velocity =
+    let
+        pos = case direction of
+            Up ->
+                position - velocity
+            Down ->
+                position + velocity
+    in
+        if pos < 0
+            then 0
+            else if pos > screen
+                then toFloat screen
+                else pos
+
+updateMove : Model -> Model
+updateMove model =
+    let
+        { position, moving, screen, velocity } = model
+    in
+        case moving of
+            Nothing ->
+                model
+            Just d ->
+                { model | position = (moveDeck position d screen velocity) }
+
+keyCodeToMoving : KeyCode -> Model -> Move
+keyCodeToMoving keyCode { upKey, downKey } =
+    if keyCode == upKey
+        then Just Up
+        else if keyCode == downKey
+            then Just Down
+            else Nothing
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Run ->
+            ({ model | moveable = True }, Cmd.none)
+        Stop ->
+            ({ model | moveable = False }, Cmd.none)
+
+        MoveStart code ->
+            (updateMove { model | moving = keyCodeToMoving code model }, Cmd.none)
+        MoveEnd code ->
+            ({ model | moving = Nothing }, Cmd.none)
+
+        Tick _ ->
+            (updateMove model, Cmd.none)
+        Screen height ->
+            ({ model | screen = height, initialized = True }, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions { moveable, initialized } =
+    if moveable && initialized
+        then Sub.batch [
+            clock Tick,
+            downs MoveStart,
+            ups MoveEnd
+        ]
+        else Sub.none
 
 view : Model -> Html Msg
-view =
-div [
-style [
-("background", "#000"),
-("width", px 10),
-("height", px 70),
-("position", "absolute"),
-("top", px 40),
-]
-]
+view { position, height, direction } =
+    div [
+        style [
+            ("background", "#000"),
+            ("width", px 10),
+            ("height", px (toFloat height)),
+            ("position", "absolute"),
+            ("top", px position),
+            case direction of
+                True ->
+                    ("left", "0")
+                False ->
+                    ("right", "0")
+        ]
+    ] []
--- Main.elm
+++ Main.elm
@@ -1,7 +1,7 @@
 import Html.App as Html
 import Html exposing (..)
 import Ball
-import Decker
+import Deck
 
 main =
     Html.program
@@ -12,23 +12,23 @@ main =
         }
 
 type alias Model =
-    { deck1: Decker.Model
-    , deck2: Decker.Model
+    { deck1: Deck.Model
+    , deck2: Deck.Model
     , ball: Ball.Model
     }
 
 type Msg =
-    Deck1 Decker.Msg
-    | Deck2 Decker.Msg
+    Deck1 Deck.Msg
+    | Deck2 Deck.Msg
     | Ball Ball.Msg
 
 init : (Model, Cmd Msg)
 init =
     let
-        (deck1M, deck1C) = Decker.init {
+        (deck1M, deck1C) = Deck.init {
             up = 87, down = 83
         } True
-        (deck2M, deck2C) = Decker.init {
+        (deck2M, deck2C) = Deck.init {
             up = 73, down = 75
         } False
         (ballM, ballC) = Ball.init
@@ -49,12 +49,12 @@ update msg model =
     case msg of
         Deck1 m ->
             let
-                (deck, effect) = (Decker.update m model.deck1)
+                (deck, effect) = (Deck.update m model.deck1)
             in
                 ({ model | deck1 = deck }, Cmd.map Deck1 effect)
         Deck2 m ->
             let
-                (deck, effect) = Decker.update m model.deck2
+                (deck, effect) = Deck.update m model.deck2
             in
                 ({ model | deck2 = deck }, Cmd.map Deck2 effect)
         Ball m ->
@@ -67,14 +67,14 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch [
         Sub.map Ball (Ball.subscriptions model.ball),
-        Sub.map Deck1 (Decker.subscriptions model.deck1),
-        Sub.map Deck2 (Decker.subscriptions model.deck2)
+        Sub.map Deck1 (Deck.subscriptions model.deck1),
+        Sub.map Deck2 (Deck.subscriptions model.deck2)
     ]
 
 view : Model -> Html Msg
 view { deck1, deck2, ball} =
     div [] [
         Html.map Ball (Ball.view ball),
-        Html.map Deck1 (Decker.view deck1),
-        Html.map Deck2 (Decker.view deck2)
+        Html.map Deck1 (Deck.view deck1),
+        Html.map Deck2 (Deck.view deck2)
     ]
